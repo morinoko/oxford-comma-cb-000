26fcf362ae08fbb13799f881543b15d32bf635d7
Felice Forby <feliceforby@gmail.com>
2018-04-14 02:38:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -20,7 +20,7 @@
 def oxford_comma(array)
   if array.size <= 1
     array.join 
-  if array.size == 2
+  elsif array.size == 2
     return array.join(" and ")
   else
     array[-1] = "and #{array[-1]}"
