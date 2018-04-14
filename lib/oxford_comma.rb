ec41b6000200fea837475d488ec03064bd168778
Felice Forby <feliceforby@gmail.com>
2018-04-14 02:40:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,30 @@
+# def oxford_comma(array)
+#   if array.size <= 1
+#     array.join 
+#   elsif array.size == 2
+#     array.join(" and ")
+#   else
+#     oxford_comma_str = ""
+#     last_index = array.index(array.last)
+#     array.each_with_index do |item, index|
+#       if index < last_index
+#         oxford_comma_str << item + ", "
+#       elsif index == last_index
+#         oxford_comma_str << "and " + item
+#       end
+#     end
+#     oxford_comma_str
+#   end
+# end
+
+# Refactored version
 def oxford_comma(array)
   if array.size <= 1
     array.join 
   elsif array.size == 2
     array.join(" and ")
   else
-    oxford_comma_str = ""
-    array.each_with_index do |item, index|
-      if index < array.index(array.last)
-        oxford_comma_str << item + ", "
-      else
-        oxford_comma_str << "and " + item
-      end
-    end
-    oxford_comma_str
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
   end
 end
