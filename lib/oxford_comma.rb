be396dbda59ca786b607bffc70a721e0565ed022
Felice Forby <feliceforby@gmail.com>
2018-04-14 02:28:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -16,3 +16,22 @@ def oxford_comma(array)
     oxford_comma_str
   end
 end
+
+def oxford_comma(array)
+  if array.size <= 1
+    array.join 
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    oxford_comma_str = ""
+    last_index = array.index(array.last)
+    array.each_with_index do |item, index|
+      if index < last_index
+        oxford_comma_str << item + ", "
+      elsif index == last_index
+        oxford_comma_str << "and " + item
+      end
+    end
+    oxford_comma_str
+  end
+end
