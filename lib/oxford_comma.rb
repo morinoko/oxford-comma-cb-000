1ab052b78c9ead40f5ca571190df6699b8d09c69
Felice Forby <feliceforby@gmail.com>
2018-04-14 02:32:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,37 +1,30 @@
-def oxford_comma(array)
-  if array.size <= 1
-    array.join 
-  elsif array.size == 2
-    array.join(" and ")
-  else
-    oxford_comma_str = ""
-    last_index = array.index(array.last)
-    array.each_with_index do |item, index|
-      if index < last_index
-        oxford_comma_str << item + ", "
-      elsif index == last_index
-        oxford_comma_str << "and " + item
-      end
-    end
-    oxford_comma_str
-  end
-end
+# def oxford_comma(array)
+#   if array.size <= 1
+#     array.join 
+#   elsif array.size == 2
+#     array.join(" and ")
+#   else
+#     oxford_comma_str = ""
+#     last_index = array.index(array.last)
+#     array.each_with_index do |item, index|
+#       if index < last_index
+#         oxford_comma_str << item + ", "
+#       elsif index == last_index
+#         oxford_comma_str << "and " + item
+#       end
+#     end
+#     oxford_comma_str
+#   end
+# end
 
 def oxford_comma(array)
   if array.size <= 1
     array.join 
   elsif array.size == 2
-    array.join(" and ")
+    return array.join(" and ")
   else
-    oxford_comma_str = ""
     last_index = array.index(array.last)
-    array.each_with_index do |item, index|
-      if index < last_index
-        oxford_comma_str << item + ", "
-      elsif index == last_index
-        oxford_comma_str << "and " + item
-      end
-    end
-    oxford_comma_str
+    array.insert(last_index - 1, "and")
+    array.join(", ")
   end
 end
